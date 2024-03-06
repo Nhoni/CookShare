a866ac0f6dca53430521d6294a74dadf1eb4d394
Nhora <118848679+Nhoni@users.noreply.github.com>
2024-03-06 01:16:30 +0100

    Mise a jour des fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,9 @@
 
 namespace App\DataFixtures;
 
-use Faker\Generator;
 use Faker\Factory;
+use Faker\Generator;
+use App\Entity\Recipe;
 use App\Entity\Ingredient;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -15,21 +16,41 @@ class AppFixtures extends Fixture
      */
     private Generator $faker;
 
-    public function __construct(){
-        $this->faker =Factory::create('fr_FR');
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
     }
 
     public function load(ObjectManager $manager): void
     {
+        // Ingredient fixtures
         for ($i = 0; $i < 50; $i++) {
             $ingredient = new Ingredient();
             $ingredient->setName($this->faker->word())
                 ->setQuantite(mt_rand(0, 100))
-                ->setUnite(mt_rand(0,100));
-            
+                ->setUnite(mt_rand(0, 100));
+
+            $ingredients[] = $ingredient;
             $manager->persist($ingredient);
         }
 
+        // Recette fixtures
+        for ($j = 0; $j < 50; $j++) {
+            $recipe = new Recipe();
+            $recipe->setName($this->faker->word())
+                ->setTime(mt_rand(0, 1) == 1 ? mt_rand(1, 1440) : null)
+                ->setNbPeople(mt_rand(0, 1) == 1 ? mt_rand(1, 50) : null)
+                ->setDifficulty(mt_rand(0, 1) == 1 ? mt_rand(1, 5) : null)
+                ->setDescription($this->faker->text(300))
+                ->setIsFavorite(mt_rand(0, 1) == 1 ? true : false);
+
+            for ($k = 0; $k < mt_rand(5, 15); $k++) {
+                $recipe->addIngredient($ingredients[mt_rand(0, count($ingredients) - 1)]);
+            }
+
+
+            $manager->persist($recipe);
+        }
 
         $manager->flush();
     }
